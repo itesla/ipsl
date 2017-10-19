itesla/ipsl

commit 4eb03d4e7b373ab05de9e7f101bf0395ab9807aa

#114 Moved and improved the common intialization routines of PSAT machines to baseMachine, removed wildcat imports.

OpenIPSL/Electrical/Machines/PSAT/BaseClasses/baseMachine.mo
@@ -2,7 +2,9 @@ within OpenIPSL.Electrical.Machines.PSAT.BaseClasses;
 partial model baseMachine
   extends OpenIPSL.Electrical.Essentials.pfComponent;
   import Modelica.Constants.pi;
-  import Modelica.SIunits.*;
+  import SI = Modelica.SIunits;
+  import CM = Modelica.ComplexMath;
+  import Complex;
   parameter Real Sn "Power rating (MVA)"
     annotation (Dialog(group="Machine parameters"));
   parameter Real Vn "Voltage rating (kV)"
@@ -11,11 +13,11 @@ partial model baseMachine
     annotation (Dialog(group="Machine parameters"));
   parameter Real x1d "d-axis transient reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real M "Machanical starting time (2H), kWs/kVA"
+  parameter Real M "Mechanical starting time (2H), kWs/kVA"
     annotation (Dialog(group="Machine parameters"));
   parameter Real D "Damping coefficient"
     annotation (Dialog(group="Machine parameters"));
-  //Initialization of currents and voltages
+
   OpenIPSL.Interfaces.PwPin p(
     vr(start=vr0),
     vi(start=vi0),
@@ -25,6 +27,7 @@ partial model baseMachine
         extent={{-10.0,-10.0},{10.0,10.0}},
         rotation=0)));
   Modelica.Blocks.Interfaces.RealOutput delta(
+    start=delta0,
     quantity="Angle",
     unit="rad",
     displayUnit="rad") "Rotor angle (rad)" annotation (Placement(visible=true,
@@ -32,19 +35,14 @@ partial model baseMachine
         origin={110,60},
         extent={{-10,-10},{10,10}},
         rotation=0)));
-  Modelica.Blocks.Interfaces.RealOutput w(
-    start=1,
-    quantity="PerUnit",
-    displayUnit="pu") "Rotor speed (pu)" annotation (Placement(visible=true,
-        transformation(
+  Modelica.Blocks.Interfaces.RealOutput w(start=1) "Rotor speed (pu)"
+    annotation (Placement(visible=true, transformation(
         origin={110,90},
         extent={{-10,-10},{10,10}},
         rotation=0)));
-  Modelica.Blocks.Interfaces.RealOutput v(
-    quantity="PerUnit",
-    displayUnit="pu",
-    start=V_0) "Generator terminal voltage (pu)" annotation (Placement(visible=
-          true,transformation(
+  Modelica.Blocks.Interfaces.RealOutput v(start=V_0)
+    "Generator terminal voltage (pu)" annotation (Placement(visible=true,
+        transformation(
         origin={110,30},
         extent={{-10,-10},{10,10}},
         rotation=0)));
@@ -68,49 +66,71 @@ partial model baseMachine
         origin={-80,110},
         extent={{-10,-10},{10,10}},
         rotation=90)));
-  Modelica.Blocks.Interfaces.RealOutput pm0 "Initial mechanical power (pu)"
-    annotation (Placement(visible=true, transformation(
+  Modelica.Blocks.Interfaces.RealOutput pm0(start=pm00)
+    "Initial mechanical power (pu)" annotation (Placement(visible=true,
+        transformation(
         origin={-80,-110},
         extent={{-10,-10},{10,10}},
         rotation=270)));
-  Modelica.Blocks.Interfaces.RealInput pm "Mechanical power (pu)" annotation (
-      Placement(visible=true, transformation(
+  Modelica.Blocks.Interfaces.RealInput pm(start=pm00) "Mechanical power (pu)"
+    annotation (Placement(visible=true, transformation(
         origin={-120,-50},
         extent={{-20,-20},{20,20}},
         rotation=0)));
-  Angle anglev(start=Conversions.from_deg(angle_0)) " Bus voltage angle (rad)";
-  Real vd "d-axis voltage (pu)";
-  Real vq "q-axis voltage (pu)";
-  Real id "d-axis currrent (pu)";
-  Real iq "q-axis current (pu)";
-protected
-  parameter Real vr0=V_0*cos(angle_0*pi/180) "Initialization (pu)";
-  parameter Real vi0=V_0*sin(angle_0*pi/180) "Initialization (pu)";
-  parameter Real ir0=-CoB*(p0*vr0 + q0*vi0)/(vr0^2 + vi0^2)
-    "Initialization (system base)";
-  parameter Real ii0=-CoB*(p0*vi0 - q0*vr0)/(vr0^2 + vi0^2)
-    "Initialitation (system base)";
-  //From machine base to system base
-  parameter Real CoB=Sn/S_b;
+  SI.Angle anglev(start=SI.Conversions.from_deg(angle_0))
+    " Bus voltage angle (rad)";
+  Real vd(start=vd0) "d-axis voltage (pu)";
+  Real vq(start=vq0) "q-axis voltage (pu)";
+  Real id(start=id0) "d-axis currrent (pu)";
+  Real iq(start=iq0) "q-axis current (pu)";
+  //protected
   parameter Real w_b=2*pi*fn "Base frequency in rad/s";
-  parameter Real p0=P_0/Sn
-    "initial active power generation in pu (machine base)";
-  parameter Real q0=Q_0/Sn
-    "initial reactive power generation in pu (machine base)";
-  parameter Real ii0mb=-ii0/CoB;
-  parameter Real ir0mb=-ir0/CoB;
+  parameter Real CoB=Sn/S_b "From machine base to system base";
+
+  // Initialize stator quantities (system base):
+protected
+  parameter Real p0=P_0/S_b
+    "Initial active power generation in pu (system base)";
+  parameter Real q0=Q_0/S_b
+    "Initial reactive power generation in pu (system base)";
+  parameter Complex Vt0=CM.fromPolar(V_0, -SI.Conversions.from_deg(angle_0))
+    "conj.";
+  parameter Complex S0(re=p0, im=-q0) "conj.";
+  parameter Complex I0=S0/(CM.conj(Vt0));
+  parameter Real vr0=CM.real(Vt0) "Initialization (pu)";
+  parameter Real vi0=CM.imag(Vt0) "Initialization (pu)";
+  parameter Real ir0=-CM.real(I0) "Initialization (pu)";
+  parameter Real ii0=-CM.imag(I0) "Initialization (pu)";
+
+  // Initialize DQ-quantities (machine base)
+
+  parameter Real xq0 "used for setting the initial rotor angle";
+  parameter SI.Angle delta0=CM.arg(Vt0 + (ra + CM.j*xq0)*(I0/CoB))
+    "Initial rotor angle";
+  parameter Complex Vdq0=Vt0*CM.fromPolar(1, -delta0 + (pi/2));
+  parameter Complex Idq0=I0/CoB*CM.fromPolar(1, -delta0 + (pi/2));
+  parameter Real vd0=CM.real(Vdq0) "Initialization (pu, systembase)";
+  parameter Real vq0=CM.imag(Vdq0) "Initialization (pu, systembase)";
+  parameter Real id0=CM.real(Idq0) "Initialization (pu, systembase)";
+  parameter Real iq0=CM.imag(Idq0) "Initialization (pu, systembase)";
+  parameter Real pm00=(vq0 + ra*iq0)*iq0 + (vd0 + ra*id0)*id0 "Initialization";
 initial equation
   w = 1;
+  der(delta) = 0;
 equation
   v = sqrt(p.vr^2 + p.vi^2);
   anglev = atan2(p.vi, p.vr);
   der(delta) = w_b*(w - 1);
-  der(w) = (pm - (P/CoB) - D*(w - 1))/M;
+  if D > Modelica.Constants.eps then
+    der(w) = (pm - (P/CoB) - D*(w - 1))/M;
+  else
+    der(w) = (pm - (P/CoB))/M;
+  end if;
   [p.ir; p.ii] = -CoB*[sin(delta), cos(delta); -cos(delta), sin(delta)]*[id; iq];
   [p.vr; p.vi] = [sin(delta), cos(delta); -cos(delta), sin(delta)]*[vd; vq];
   -P = p.vr*p.ir + p.vi*p.ii;
   -Q = p.vi*p.ir - p.vr*p.ii;
-
+  pm0 = pm00;
   annotation (
     Icon(coordinateSystem(extent={{-100,-100},{100,100}}, initialScale=0.1),
         graphics={

OpenIPSL/Electrical/Machines/PSAT/Order2.mo
@@ -1,37 +1,20 @@
 within OpenIPSL.Electrical.Machines.PSAT;
 model Order2 "Second Order Synchronous Machine with Inputs and Outputs"
-  import Modelica.Constants.pi;
   extends BaseClasses.baseMachine(
-    delta(start=delta0),
-    pm(start=pm00),
-    pm0(start=pm00),
     vf(start=vf00),
     vf0(start=vf00),
-    vq(start=vq0),
-    vd(start=vd0),
-    iq(start=iq0),
-    id(start=id0));
+    xq0=x1d);
 protected
-  parameter Real c1=ra*K "CONSTANT";
-  parameter Real c2=x1d*K "CONSTANT";
-  parameter Real c3=x1d*K " CONSTANT";
-  parameter Real K=1/(ra*ra + x1d*x1d) "CONSTANT";
-  parameter Real delta0=atan2(vi0 + ra*ii0mb + x1d*ir0mb, vr0 + ra*ir0mb - x1d*
-      ii0mb) "Initial rotor angle";
-  parameter Real pm00=(vq0 + ra*iq0)*iq0 + (vd0 + ra*id0)*id0 "Initialitation";
-  parameter Real vf00=vq0 + ra*iq0 + x1d*id0 "Initialitation";
-  parameter Real vd0=vr0*cos(pi/2 - delta0) - vi0*sin(pi/2 - delta0)
-    "Initialitation";
-  parameter Real vq0=vr0*sin(pi/2 - delta0) + vi0*cos(pi/2 - delta0)
-    "Initialitation";
-  parameter Real id0=ir0mb*cos(pi/2 - delta0) - ii0mb*sin(pi/2 - delta0)
-    "Initialitation";
-  parameter Real iq0=ir0mb*sin(pi/2 - delta0) + ii0mb*cos(pi/2 - delta0)
-    "Initialitation";
+  parameter Real K=1/(ra^2 + x1d^2) "a constant for scaling";
+  parameter Real c1=ra*K "scaled ra";
+  parameter Real c2=x1d*K "scaled x'd";
+  parameter Real c3=x1d*K "scaled x'd";
+  parameter Real vf00=vq0 + ra*iq0 + x1d*id0 "Initialization";
+initial equation
+  vf0 = vf00;
 equation
-  id = (-c1*vd) - c3*vq + vf*c3;
+  id = -c1*vd + c3*vq + vf*c3;
   iq = c2*vd - c1*vq + vf*c1;
-  pm0 = pm00;
   vf0 = vf00;
   annotation (Documentation(info="<html> 
 <table cellspacing=\"1\" cellpadding=\"1\" border=\"1\"> 

OpenIPSL/Electrical/Machines/PSAT/Order3.mo
@@ -1,52 +1,31 @@
 within OpenIPSL.Electrical.Machines.PSAT;
 model Order3 "Third Order Synchronous Machine with Inputs and Outputs"
-  import Modelica.Constants.pi;
   extends BaseClasses.baseMachine(
-    delta(start=delta0),
-    pm(start=pm00),
-    pm0(start=pm00),
     vf(start=vf00),
     vf0(start=vf00),
-    vq(start=vq0),
-    vd(start=vd0),
-    iq(start=iq0),
-    id(start=id0));
+    xq0=xq);
   parameter Real xd "d-axis synchronous reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real Td10 "d-axis open circuit transient time constant (s)"
+  parameter Real T1d0 "d-axis open circuit transient time constant (s)"
     annotation (Dialog(group="Machine parameters"));
   parameter Real xq "q-axis synchronous reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
-  //Base changing
   Real e1q(start=e1q0) "q-axis transient voltage (pu)";
+
 protected
-  parameter Real Xd=xd*CoB "d-axis reactance, p.u.";
-  parameter Real Xq=xq*CoB "q-axis reactance, p.u.";
-  parameter Real c1=ra*K "CONSTANT";
-  parameter Real c2=x1d*K "CONSTANT";
-  parameter Real c3=Xq*K " CONSTANT";
-  parameter Real K=1/(ra*ra + Xq*x1d) "CONSTANT";
-  parameter Real delta0=atan2(vi0 + ra*ii0 + Xq*ir0, vr0 + ra*ir0 - Xq*ii0)
-    "Initialitation";
-  parameter Real vd0=vr0*cos(pi/2 - delta0) - vi0*sin(pi/2 - delta0)
-    "Initialitation";
-  parameter Real vq0=vr0*sin(pi/2 - delta0) + vi0*cos(pi/2 - delta0)
-    "Initialitation";
-  parameter Real id0=ir0*cos(pi/2 - delta0) - ii0*sin(pi/2 - delta0)
-    "Initialitation";
-  parameter Real iq0=ir0*sin(pi/2 - delta0) + ii0*cos(pi/2 - delta0)
-    "Initialitation";
-  parameter Real pm00=(vq0 + ra*iq0)*iq0 + (vd0 + ra*id0)*id0 "Initialitation";
-  parameter Real vf00=e1q0 + (Xd - x1d)*id0 "Initialitation";
+  parameter Real K=1/(ra^2 + xq*x1d) "a constant for scaling";
+  parameter Real c1=ra*K "scaled ra";
+  parameter Real c2=x1d*K "scaled x'd";
+  parameter Real c3=xq*K " scaled xq";
+  parameter Real vf00=e1q0 + (xd - x1d)*id0 "Initialitation";
   parameter Real e1q0=vq0 + ra*iq0 + x1d*id0 "Initialitation";
 initial equation
   der(e1q) = 0;
 equation
-  der(e1q) = ((-e1q) - (Xd - x1d)*id + vf)/Td10;
+  der(e1q) = ((-e1q) - (xd - x1d)*id + vf)/T1d0;
   id = (-c1*vd) - c3*vq + e1q*c3;
   iq = c2*vd - c1*vq + e1q*c1;
   vf0 = vf00;
-  pm0 = pm00;
   annotation (Icon(coordinateSystem(extent={{-100,-100},{100,100}},
           initialScale=0.1), graphics={Text(
           origin={0,60},

OpenIPSL/Electrical/Machines/PSAT/Order4.mo
@@ -1,53 +1,34 @@
 within OpenIPSL.Electrical.Machines.PSAT;
 model Order4 "Fourth Order Synchronous Machine with Inputs and Outputs"
-  import Modelica.Constants.pi;
   extends BaseClasses.baseMachine(
-    delta(start=delta0),
-    pm(start=pm00),
-    pm0(start=pm00),
     vf(start=vf00),
     vf0(start=vf00),
-    vq(start=vq0),
-    vd(start=vd0),
-    iq(start=iq0),
-    id(start=id0));
+    xq0=xq);
   parameter Real xd=1.9 "d-axis synchronous reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
   parameter Real xq=1.7 "q-axis synchronous reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real xq1=0.5 "q-axis transient reactance (pu)"
+  parameter Real x1q=0.5 "q-axis transient reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real Td10=8 "d-axis open circuit transient time constant (s)"
+  parameter Real T1d0=8 "d-axis open circuit transient time constant (s)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real Tq10=0.8 "q-axis open circuit transient time constant (s)"
+  parameter Real T1q0=0.8 "q-axis open circuit transient time constant (s)"
     annotation (Dialog(group="Machine parameters"));
   Real e1q(start=e1q0) "q-axis transient voltage (pu)";
   Real e1d(start=e1d0) "d-axis transient voltage (pu)";
 protected
-  parameter Real Xd=xd*CoB "d-axis reactance, p.u.";
-  parameter Real Xq=xq*CoB "q-axis reactance, p.u.";
-  parameter Real x1q=xq1*CoB
-    "q-axis transient reactance, p.u.(only for order IV,V.1,VI,VIII)*";
-  parameter Real vd0=vr0*cos(pi/2 - delta0) - vi0*sin(pi/2 - delta0)
-    "Initialitation";
-  parameter Real vq0=vr0*sin(pi/2 - delta0) + vi0*cos(pi/2 - delta0)
-    "Initialitation";
-  parameter Real id0=ir0*cos(pi/2 - delta0) - ii0*sin(pi/2 - delta0)
-    "Initialitation";
-  parameter Real iq0=ir0*sin(pi/2 - delta0) + ii0*cos(pi/2 - delta0)
-    "Initialitation";
-  parameter Real delta0=atan2(vi0 + ra*ii0 + Xq*ir0, vr0 + ra*ir0 - Xq*ii0)
-    "Initialitation";
-  parameter Real pm00=(vq0 + ra*iq0)*iq0 + (vd0 + ra*id0)*id0 "Initialitation";
-  parameter Real vf00=e1q0 + (Xd - x1d)*id0 "Initialitation";
-  parameter Real e1q0=vq0 + ra*iq0 + x1d*id0 "Initialitation";
-  parameter Real e1d0=vd0 + ra*id0 - x1q*iq0 "Initialitation *";
+  parameter Real vf00=e1q0 + (xd - x1d)*id0 "Initialization";
+  parameter Real e1q0=vq0 + ra*iq0 + x1d*id0 "Initialization";
+  parameter Real e1d0=vd0 + ra*id0 - x1q*iq0 "Initialization";
+initial equation
+  der(e1q) = 0;
+  der(e1d) = 0;
+
 equation
-  der(e1q) = ((-e1q) - (Xd - x1d)*id + vf)/Td10;
-  der(e1d) = ((-e1d) + (Xq - x1q)*iq)/Tq10 "differential equations *";
+  der(e1q) = ((-e1q) - (xd - x1d)*id + vf)/T1d0;
+  der(e1d) = ((-e1d) + (xq - x1q)*iq)/T1q0 "differential equations *";
   e1q = vq + ra*iq + x1d*id;
   e1d = vd + ra*id - x1q*iq "relation between voltages and currents *";
-  pm0 = pm00;
   vf0 = vf00;
   annotation (Icon(coordinateSystem(extent={{-100,-100},{100,100}},
           initialScale=0.1), graphics={Text(

OpenIPSL/Electrical/Machines/PSAT/Order5_Type1.mo
@@ -1,61 +1,44 @@
 within OpenIPSL.Electrical.Machines.PSAT;
 model Order5_Type1
-  import Modelica.Constants.pi;
   extends BaseClasses.baseMachine(
-    delta(start=delta0),
-    pm(start=pm00),
-    pm0(start=pm00),
     vf(start=vf00),
     vf0(start=vf00),
-    vq(start=vq0),
-    vd(start=vd0),
-    iq(start=iq0),
-    id(start=id0));
+    xq0=xq);
   parameter Real xd=1.9 "d-axis synchronous reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
   parameter Real xq=1.7 "q-axis synchronous reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real xq1=0.5 "q-axis transient reactance (pu)"
+  parameter Real x1q=0.5 "q-axis transient reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real Td10=8 "d-axis open circuit transient time constant (s)"
+  parameter Real T1d0=8 "d-axis open circuit transient time constant (s)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real Tq10=0.8 "q-axis open circuit transient time constant (s)"
+  parameter Real T1q0=0.8 "q-axis open circuit transient time constant (s)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real Tq20=0.02
+  parameter Real T2q0=0.02
     "q-axis open circuit sub-transient time constant (s)"
     annotation (Dialog(group="Machine parameters"));
   Real e1q(start=e1q0) "q-axis transient voltage";
   Real e1d(start=e1d0) "d-axis transient voltage";
   Real e2d(start=e2d0) "d-axis sub-transient voltage";
 protected
-  parameter Real Xd=xd*CoB "d-axis reactance, p.u.";
-  parameter Real Xq=xq*CoB "q-axis reactance, p.u.";
-  parameter Real x1q=xq1*CoB
-    "q-axis transient reactance, p.u.(only for order IV,V.1,VI,VIII)*";
-  parameter Real delta0=atan2(vi0 + ra*ii0 + Xq*ir0, vr0 + ra*ir0 - Xq*ii0)
-    "Initialitation";
-  parameter Real vd0=vr0*cos(pi/2 - delta0) - vi0*sin(pi/2 - delta0)
-    "Initialitation";
-  parameter Real vq0=vr0*sin(pi/2 - delta0) + vi0*cos(pi/2 - delta0)
-    "Initialitation";
-  parameter Real id0=ir0*cos(pi/2 - delta0) - ii0*sin(pi/2 - delta0)
-    "Initialitation";
-  parameter Real iq0=ir0*sin(pi/2 - delta0) + ii0*cos(pi/2 - delta0)
-    "Initialitation";
-  parameter Real pm00=(vq0 + ra*iq0)*iq0 + (vd0 + ra*id0)*id0 "Initialitation";
-  parameter Real vf00=e1q0 + (Xd - x1d)*id0 "Initialitation";
-  parameter Real e1q0=vq0 + ra*iq0 + x1d*id0 "Initialitation";
-  parameter Real e1d0=vd0 + ra*id0 - x1q*iq0 "Initialitation*";
-  parameter Real e2d0=vd0 + ra*id0 - x1q*iq0 "Initialitation*/";
+  parameter Real vf00=e1q0 + (xd - x1d)*id0 "Initialization";
+  parameter Real e1q0=vq0 + ra*iq0 + x1d*id0 "Initialization";
+  parameter Real e1d0=vd0 + ra*id0 - x1q*iq0 "Initialization*";
+  parameter Real e2d0=vd0 + ra*id0 - x1q*iq0 "Initialization";
+initial equation
+  der(delta) = 0;
+  der(e1q) = 0;
+  der(e1d) = 0;
+  der(e2d) = 0;
 equation
-  der(e1q) = ((-e1q) - (Xd - x1d)*id + vf)/Td10;
-  der(e1d) = ((-e1d) + (Xq - x1q - Tq20/Tq10*x1d/x1q*(Xq - x1q))*iq)/Tq10
+  der(e1q) = ((-e1q) - (xd - x1d)*id + vf)/T1d0;
+  der(e1d) = ((-e1d) + (xq - x1q - T2q0/T1q0*x1d/x1q*(xq - x1q))*iq)/T1q0
     "differential equations/";
-  der(e2d) = ((-e2d) + e1d + (x1q - x1d + Tq20/Tq10*x1d/x1q*(Xq - x1q))*iq)/
-    Tq20 "differential equations/";
+  der(e2d) = ((-e2d) + e1d + (x1q - x1d + T2q0/T1q0*x1d/x1q*(xq - x1q))*iq)/
+    T2q0 "differential equations/";
   e1q = vq + ra*iq + x1d*id;
   e2d = vd + ra*id - x1q*iq "relation between voltages and currents/";
-  pm0 = pm00;
+
   vf0 = vf00;
   annotation (Icon(coordinateSystem(extent={{-100,-100},{100,100}},
           initialScale=0.1), graphics={Text(

OpenIPSL/Electrical/Machines/PSAT/Order5_Type2.mo
@@ -1,29 +1,22 @@
 within OpenIPSL.Electrical.Machines.PSAT;
 model Order5_Type2
-  import Modelica.Constants.pi;
   extends BaseClasses.baseMachine(
-    delta(start=delta0),
-    pm(start=pm00),
-    pm0(start=pm00),
     vf(start=vf00),
     vf0(start=vf00),
-    vq(start=vq0),
-    vd(start=vd0),
-    iq(start=iq0),
-    id(start=id0));
+    xq0=xq);
   parameter Real xd=1.9 "d-axis synchronous reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
   parameter Real xq=1.7 "q-axis synchronous reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real xd2=0.204 "d-axis sub-transient reactance (pu)"
+  parameter Real x2d=0.204 "d-axis sub-transient reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real xq2=0.3 "q-axis sub-transient reactance (pu)"
+  parameter Real x2q=0.3 "q-axis sub-transient reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real Td10=8 "d-axis open circuit transient time constant (s)"
+  parameter Real T1d0=8 "d-axis open circuit transient time constant (s)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real Td20=0.8 "d-axis open circuit transient time constant (s)"
+  parameter Real T2d0=0.8 "d-axis open circuit transient time constant (s)"
     annotation (Dialog(group="Machine parameters"));
-  parameter Real Tq20=0.02 "q-axis open circuit transient time constant (s)"
+  parameter Real T2q0=0.02 "q-axis open circuit transient time constant (s)"
     annotation (Dialog(group="Machine parameters"));
   parameter Real Taa=0.002 "d-axis aditional leakage time constant (s)"
     annotation (Dialog(group="Machine parameters"));
@@ -31,38 +24,24 @@ model Order5_Type2
   Real e2q(start=e2q0) "q-axis sub-transient voltage";
   Real e2d(start=e2d0) "d-axis sub-transient voltage";
 protected
-  parameter Real Xd=xd*CoB "d-axis reactance, p.u.";
-  parameter Real x2d=xd2*CoB
-    "d-axis sub-transient reactance, p.u.(only for V.2,VI,VIII)";
-  parameter Real Xq=xq*CoB "q-axis reactance, p.u.";
-  parameter Real x2q=xq2*CoB
-    "q-axis sub-trasient reactance, p.u.(only for V.2,VI,VIII)";
-  parameter Real delta0=atan2(vi0 + ra*ii0 + Xq*ir0, vr0 + ra*ir0 - Xq*ii0)
-    "Initialitation";
-  parameter Real vd0=vr0*cos(pi/2 - delta0) - vi0*sin(pi/2 - delta0)
-    "Initialitation";
-  parameter Real vq0=vr0*sin(pi/2 - delta0) + vi0*cos(pi/2 - delta0)
-    "Initialitation";
-  parameter Real id0=ir0*cos(pi/2 - delta0) - ii0*sin(pi/2 - delta0)
-    "Initialitation";
-  parameter Real iq0=ir0*sin(pi/2 - delta0) + ii0*cos(pi/2 - delta0)
-    "Initialitation";
-  parameter Real pm00=(vq0 + ra*iq0)*iq0 + (vd0 + ra*id0)*id0 "Initialitation";
   parameter Real e2q0=vq0 + ra*iq0 + x2d*id0 "Initialitation";
   parameter Real e2d0=vd0 + ra*id0 - x2q*iq0 "Initialitation";
-  parameter Real K1=Xd - x1d - Td20/Td10*x2d/x1d*(Xd - x1d);
-  parameter Real K2=x1d - x2d + Td20/Td10*x2d/x1d*(Xd - x1d);
-  parameter Real e1q0=(-K1*Taa/Td10*id0) + (1 - Taa/Td10)*(e2q0 + K2*id0);
-  parameter Real vf00=(K1*id0 + e1q0)/(1 - Taa/Td10);
+  parameter Real K1=xd - x1d - T2d0/T1d0*x2d/x1d*(xd - x1d);
+  parameter Real K2=x1d - x2d + T2d0/T1d0*x2d/x1d*(xd - x1d);
+  parameter Real e1q0=(-K1*Taa/T1d0*id0) + (1 - Taa/T1d0)*(e2q0 + K2*id0);
+  parameter Real vf00=(K1*id0 + e1q0)/(1 - Taa/T1d0);
+initial equation
+  der(e1q) = 0;
+  der(e2q) = 0;
+  der(e2d) = 0;
 equation
-  der(e1q) = ((-e1q) - (Xd - x1d - Td20/Td10*x2d/x1d*(Xd - x1d))*id + (1 - Taa/
-    Td10)*vf)/Td10;
-  der(e2d) = ((-e2d) + (Xq - x2q)*iq)/Tq20;
-  der(e2q) = ((-e2q) + e1q - (x1d - x2d + Td20/Td10*x2d/x1d*(Xd - x1d))*id +
-    Taa/Td10*vf)/Td20 "differential equations";
+  der(e1q) = ((-e1q) - (xd - x1d - T2d0/T1d0*x2d/x1d*(xd - x1d))*id + (1 - Taa/
+    T1d0)*vf)/T1d0;
+  der(e2d) = ((-e2d) + (xq - x2q)*iq)/T2q0;
+  der(e2q) = ((-e2q) + e1q - (x1d - x2d + T2d0/T1d0*x2d/x1d*(xd - x1d))*id +
+    Taa/T1d0*vf)/T2d0 "differential equations";
   e2q = vq + ra*iq + x2d*id;
   e2d = vd + ra*id - x2q*iq "relation between voltages and currents";
-  pm0 = pm00;
   vf0 = vf00;
   annotation (Icon(coordinateSystem(extent={{-100,-100},{100,100}},
           initialScale=0.1), graphics={Rectangle(

OpenIPSL/Electrical/Machines/PSAT/Order6.mo
@@ -1,17 +1,9 @@
 within OpenIPSL.Electrical.Machines.PSAT;
 model Order6
-  import Modelica.Constants.pi;
-  import Modelica.SIunits.*;
   extends BaseClasses.baseMachine(
-    delta(start=delta0),
-    pm(start=pm00),
-    pm0(start=pm00),
     vf(start=vf00),
     vf0(start=vf00),
-    vq(start=vq0),
-    vd(start=vd0),
-    iq(start=iq0),
-    id(start=id0));
+    xq0=xq);
   parameter Real xd=1.9 "d-axis synchronous reactance (pu)"
     annotation (Dialog(group="Machine parameters"));
   parameter Real xq=1.7 "q-axis synchronous reactance (pu)"
@@ -38,18 +30,6 @@ model Order6
   Real e2q(start=e2q0) "q-axis sub-transient voltage";
   Real e2d(start=e2d0) "d-axis sub-transient voltage";
 
-  parameter Angle delta0=atan2(vi0 + ra*ii0mb + xq*ir0mb, vr0 + ra*ir0mb - xq*
-      ii0mb) "Initialization (rad)";
-  parameter Real vd0=vr0*cos(pi/2 - delta0) - vi0*sin(pi/2 - delta0)
-    "Initialization";
-  parameter Real vq0=vr0*sin(pi/2 - delta0) + vi0*cos(pi/2 - delta0)
-    "Initialization";
-  parameter Real id0=(ir0mb*cos(pi/2 - delta0) - ii0mb*sin(pi/2 - delta0))
-    "Initialization";
-  parameter Real iq0=(ir0mb*sin(pi/2 - delta0) + ii0mb*cos(pi/2 - delta0))
-    "Initialization";
-
-  parameter Real pm00=(vq0 + ra*iq0)*iq0 + (vd0 + ra*id0)*id0 "Initialitation";
   parameter Real e2q0=vq0 + ra*iq0 + x2d*id0 "Initialitation";
   parameter Real e2d0=vd0 + ra*id0 - x2q*iq0 "Initialitation";
   parameter Real e1d0=(xq - x1q - T2q0/T1q0*x2q/x1q*(xq - x1q))*iq0;
@@ -58,7 +38,6 @@ model Order6
   parameter Real e1q0=e2q0 + K2*id0 - Taa/T1d0*((K1 + K2)*id0 + e2q0);
   parameter Real vf00=(K1*id0 + e1q0)/(1 - Taa/T1d0);
 initial equation
-  delta = delta0;
   der(e1q) = 0;
   der(e1d) = 0;
   der(e2q) = 0;
@@ -74,7 +53,6 @@ equation
     Taa/T1d0*vf)/T2d0;
   e2q = vq + ra*iq + x2d*id;
   e2d = vd + ra*id - x2q*iq;
-  pm0 = pm00;
   vf0 = vf00;
   annotation (Icon(coordinateSystem(extent={{-100,-100},{100,100}},
           initialScale=0.1), graphics={Rectangle(

commit dda9cac3445914c43681da23e42a3efbc4bdae5c

Corrects the erroneous base-quantities in PSAT machines which have been calculated in the wrong way since the first commits. Also fixes bug in PSAT machine of order 2, introduced in 4eb03d4 which made the machine behave incorrectly.

OpenIPSL/Electrical/Machines/PSAT/BaseClasses/baseMachine.mo
@@ -84,55 +84,64 @@ partial model baseMachine
   Real id(start=id0) "d-axis currrent (pu)";
   Real iq(start=iq0) "q-axis current (pu)";
 protected
+  Real pe(start=pm00) "electrical power transmitted through the air-gap";
   parameter Real w_b=2*pi*fn "Base frequency in rad/s";
-  parameter Real CoB=Sn/S_b "From machine base to system base";
+  // Define multiplicative transforms to go from one pu-base to another:
+  parameter Real S_SBtoMB=S_b/Sn "S(system base) -> S(machine base)";
+  parameter Real I_MBtoSB=(Sn*V_b)/(S_b*Vn) "I(machine base) -> I(system base)";
+  parameter Real V_MBtoSB=Vn/V_b "V(machine base) -> V(system base)";
+  parameter Real Z_MBtoSB=(S_b*Vn^2)/(Sn*V_b^2)
+    "Z(machine base) -> S(system base)";
 
   // Initialize stator quantities (system base):
-  //protected
   parameter Real p0=P_0/S_b
     "Initial active power generation in pu (system base)";
   parameter Real q0=Q_0/S_b
     "Initial reactive power generation in pu (system base)";
   parameter Complex Vt0=CM.fromPolar(V_0, SI.Conversions.from_deg(angle_0))
-    "conj.";
-  parameter Complex S0(re=p0, im=-q0) "conj.";
-  parameter Complex I0=S0/(CM.conj(Vt0));
-  parameter Real vr0=CM.real(Vt0) "Initialization (pu)";
-  parameter Real vi0=CM.imag(Vt0) "Initialization (pu)";
-  parameter Real ir0=-CM.real(I0) "Initialization (pu)";
-  parameter Real ii0=-CM.imag(I0) "Initialization (pu)";
+    "Init. val., conjugate";
+  parameter Complex S0(re=p0, im=-q0) "Init. val., conjugate";
+  parameter Complex I0=S0/(CM.conj(Vt0)) "Init. val., conjugate";
+  parameter Real vr0=CM.real(Vt0) "Init. val.";
+  parameter Real vi0=CM.imag(Vt0) "Init. val.";
+  parameter Real ir0=-CM.real(I0) "Init. val.";
+  parameter Real ii0=-CM.imag(I0) "Init. val.";
 
-  // Initialize DQ-quantities (machine base)
-
+  // Initialize DQ-quantities (pu, machine base)
   parameter Real xq0 "used for setting the initial rotor angle";
-  parameter SI.Angle delta0=CM.arg(Vt0 + (ra + CM.j*xq0)*(I0/CoB))
-    "Initial rotor angle";
-  parameter Complex Vdq0=Vt0*CM.fromPolar(1, -delta0 + (pi/2));
-  parameter Complex Idq0=I0/CoB*CM.fromPolar(1, -delta0 + (pi/2));
-  parameter Real vd0=CM.real(Vdq0) "Initialization (pu, machine base)";
-  parameter Real vq0=CM.imag(Vdq0) "Initialization (pu, machine base)";
-  parameter Real id0=CM.real(Idq0) "Initialization (pu, machine base)";
-  parameter Real iq0=CM.imag(Idq0) "Initialization (pu, machine base)";
-  parameter Real pm00=(vq0 + ra*iq0)*iq0 + (vd0 + ra*id0)*id0
-    "Initialization (pu, machine base";
+  parameter SI.Angle delta0=CM.arg((Vt0 + ((ra + CM.j*xq0)*Z_MBtoSB*I0)))
+    "Init. val. rotor angle";
+  parameter Complex Vdq0=Vt0*CM.fromPolar(1/V_MBtoSB, (-delta0 + (pi/2)))
+    "Init. val (pu, machine base)";
+  parameter Complex Idq0=I0*CM.fromPolar(1/I_MBtoSB, (-delta0 + (pi/2)))
+    "(pu, machine base)";
+  parameter Real vd0=CM.real(Vdq0) "Init. val.";
+  parameter Real vq0=CM.imag(Vdq0) "Init. val.";
+  parameter Real id0=CM.real(Idq0) "Init. val.";
+  parameter Real iq0=CM.imag(Idq0) "Init. val.";
+
+  parameter Real pm00=((vq0 + ra*iq0)*iq0 + (vd0 + ra*id0)*id0)/S_SBtoMB
+    "Init. val. (pu, system base)";
 initial equation
   w = 1;
-  der(delta) = 0;
   delta = delta0;
 equation
   v = sqrt(p.vr^2 + p.vi^2);
   anglev = atan2(p.vi, p.vr);
   der(delta) = w_b*(w - 1);
   if D > Modelica.Constants.eps then
-    der(w) = ((pm - P)/CoB - D*(w - 1))/M;
+    der(w) = (pm*S_SBtoMB - pe - D*(w - 1))/M;
   else
-    der(w) = ((pm - P)/CoB)/M;
+    der(w) = (pm*S_SBtoMB - pe)/M;
   end if;
-  [p.ir; p.ii] = -CoB*[sin(delta), cos(delta); -cos(delta), sin(delta)]*[id; iq];
-  [p.vr; p.vi] = [sin(delta), cos(delta); -cos(delta), sin(delta)]*[vd; vq];
-  -P = p.vr*p.ir + p.vi*p.ii;
-  -Q = p.vi*p.ir - p.vr*p.ii;
-  pm0 = CoB*pm00 "pu, system base";
+  [p.ir; p.ii] = -[sin(delta), cos(delta); -cos(delta), sin(delta)]*[id; iq]*
+    I_MBtoSB;
+  [p.vr; p.vi] = [sin(delta), cos(delta); -cos(delta), sin(delta)]*[vd; vq]*
+    V_MBtoSB;
+  P = -p.vr*p.ir - p.vi*p.ii;
+  Q = -p.vi*p.ir + p.vr*p.ii;
+  pe = (vq + ra*iq)*iq + (vd + ra*id)*id "pu, machine base";
+  pm0 = pm00 "pu, system base";
   annotation (
     Icon(coordinateSystem(extent={{-100,-100},{100,100}}, initialScale=0.1),
         graphics={

OpenIPSL/Electrical/Machines/PSAT/Order2.mo
@@ -1,19 +1,14 @@
 within OpenIPSL.Electrical.Machines.PSAT;
 model Order2 "Second Order Synchronous Machine with Inputs and Outputs"
-  extends BaseClasses.baseMachine(
-    vf(start=vf00),
-    vf0(start=vf00),
-    xq0=x1d);
+  extends BaseClasses.baseMachine(vf(start=vf00), xq0=x1d);
 protected
   parameter Real K=1/(ra^2 + x1d^2) "a constant for scaling";
   parameter Real c1=ra*K "scaled ra";
   parameter Real c2=x1d*K "scaled x'd";
   parameter Real c3=x1d*K "scaled x'd";
   parameter Real vf00=vq0 + ra*iq0 + x1d*id0 "Initialization";
-initial equation
-  vf0 = vf00;
 equation
-  id = -c1*vd + c3*vq + vf*c3;
+  id = -c1*vd - c3*vq + vf*c3;
   iq = c2*vd - c1*vq + vf*c1;
   vf0 = vf00;
   annotation (Documentation(info="<html> 