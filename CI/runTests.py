4aa77019a928c98a321a0ce243268e578ab48f93
Maxime Baudette <baudette@kth.se>
2017-10-24 17:24:12 +0200

    Test

--- CI/runTests.py
+++ CI/runTests.py
@@ -68,8 +68,8 @@ appExamples = {
 "AKD":"/ApplicationExamples/AKD/package.mo",
 "N44":"/ApplicationExamples/N44/package.mo",
 "OpenCPSD5d3B":"/ApplicationExamples/OpenCPSD5d3B/package.mo",
-"ThreePhase":"/ApplicationExamples/ThreePhase/package.mo",
-"RaPIdExperiments":"/ApplicationExamples/RaPIdExperiments/package.mo"
+"RaPIdExperiments":"/ApplicationExamples/RaPIdExperiments/package.mo",
+"ThreePhase":"/ApplicationExamples/ThreePhase/package.mo"
 }
 
 for package in appExamples.keys():
