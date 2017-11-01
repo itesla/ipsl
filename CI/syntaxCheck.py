d56ffde056a7758539ce834943ceb0f656e795a8
Maxime Baudette <baudette@kth.se>
2017-11-01 23:28:13 +0100

    Fix missing test probably got removed in my clumsy merge

--- CI/syntaxCheck.py
+++ CI/syntaxCheck.py
@@ -11,6 +11,7 @@ appExamples = {
 "AKD":"/ApplicationExamples/AKD/package.mo",
 "N44":"/ApplicationExamples/N44/package.mo",
 "OpenCPSD5d3B":"/ApplicationExamples/OpenCPSD5d3B/package.mo",
+"RaPIdExperiments":"/ApplicationExamples/RaPIdExperiments/package.mo"
 }
 
 # Instance of CITests
